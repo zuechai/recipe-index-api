f63aa8d92cbed5565ee2b56de307ecc9a43e2220
zuechai <zuechai@gmail.com>
2023-03-15 11:00:21 -0400

    Install packages as part of implementing passport local strategy

--- src/server.js
+++ src/server.js
@@ -1,14 +1,41 @@
 // import
 const express = require("express");
-const cors = require("cors");
 const app = express();
+const cors = require("cors");
+const helmet = require("helmet");
+const passport = require("passport");
+const session = require("express-session");
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
+const cookieParser = require("cookie-parser");
+const prisma = require("./prisma");
 const logger = require("./utils/logger/logger");
 
 // initialize
 require("dotenv").config();
-app.use(express.json());
+app.use(helmet());
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use("/static", express.static("public"));
+
+// passport
+app.use(
+  session({
+    cookie: { secure: true },
+    secret: process.env.SECRET,
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(prisma, {
+      checkPeriod: 2 * 60 * 1000, // checks every 10 minutes,
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  })
+);
+app.use(passport.authenticate("session"));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/", (_req, res) => {
   res.json("Welcome to Recipe Index API");
